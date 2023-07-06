87fcfe0b5e4e7704dd25216555c83a6e8ae63df4
Eidy Sipion <eidysipion17@gmail.com>
2023-07-06 08:37:44 -0500

    agregando el laberinto

--- Proyecto_Integrador.py
+++ Proyecto_Integrador.py
@@ -1,2 +1,20 @@
 nombre = input("ingrese el nombre de el jugador")
-print("¡Bienvenid@!",nombre)
+print("¡Bienvenid@! al juego ," , nombre)
+
+import  readchar 
+
+def main():
+    while True:
+        # Leer el carácter presionado
+        key = readchar.readchar()
+        
+        # Imprimir el carácter presionado
+        print(key)
+        
+        # Verificar si el carácter presionado es la flecha hacia arriba
+        if key =='W':  # Código de escape para la flecha hacia arriba
+            break
+
+if __name__ == '__main__':
+    main()
+
