6a48073086c98c7c674cfc4ad8c040c9d5c386d1
Eidy Sipion <eidysipion17@gmail.com>
2023-07-11 14:54:43 -0500

    agregando parte 3 del proyecto

--- Proyecto_Integrador.py
+++ Proyecto_Integrador.py
@@ -1,6 +1,9 @@
+
+# PARTE  1:
 nombre = input("ingrese el nombre de el jugador")
 print("¡Bienvenid@! al juego ," , nombre)
 
+ # PARTE  2:
 import  readchar 
 
 def main():
@@ -18,3 +21,22 @@ def main():
 if __name__ == '__main__':
     main()
 
+ # PARTE  3:
+import os
+import msvcrt
+
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def main():
+    number = 0
+
+    while number <= 50:
+        clear_terminal()
+        print(number)
+        
+        if msvcrt.kbhit():
+            key = msvcrt.getch()
+            
+            if key == b'n':
+                number += 1
